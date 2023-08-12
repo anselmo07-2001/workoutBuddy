90a148b7d643610d5a826bb3861f808cfc62d0f2
Anselmo Rivera III <rivera.anselmo.2001@gmail.com>
2023-08-12 15:29:39 +0700

    connect the app to the db

---
+++ backend/routes/workout.js
@@ -0,0 +1,39 @@
+const express = require("express")
+
+
+// /api/workouts
+const router = express.Router()
+
+router.get("/", (req,res) => {
+    res.json({
+        msg: "Get All workouts"
+    })
+    
+})
+
+router.get("/:id", (req,res) => {
+    res.json({
+        msg:"Get workout"
+    })
+})
+
+router.post("/", (req,res) => {
+    res.json({
+        msg:"create workout"
+    })
+})
+
+router.patch("/:id", (req,res) => {
+    res.json({
+        msg:"update workout"
+    })
+})
+
+router.delete("/:id", (req,res) => {
+    res.json({
+        msg:"delete workout"
+    })
+})
+
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 
 const express = require("express")
+const mongoose = require("mongoose")
 const workoutRoutes = require("./routes/workout")
 
 //express app
@@ -18,7 +19,17 @@ app.use((req,res,next) => {
 //routes
 app.use("/api/workouts", workoutRoutes)
 
-//listen for request
-app.listen(process.env.PORT, () => {
-    console.log("listen to port ", process.env.PORT)
-})
+
+//connect to db
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true } )
+    .then(() => {
+        //listen for request
+        app.listen(process.env.PORT, () => {
+            console.log("connected to db and listen to port ", process.env.PORT)
+        })
+    })
+    .catch((err) => {
+        console.log("cannot connect to DB ", err)
+    })
+
+
