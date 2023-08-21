299ae5ca934345438df3e210bf95f5af090e3acf
Anselmo Rivera III <rivera.anselmo.2001@gmail.com>
2023-08-21 13:46:20 +0700

    implement rtk query to handle async operation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,28 +1,14 @@
-import { useState, useEffect } from "react"
-import axios from "axios"
+import { useFetchWorkoutsQuery } from "../store"
 import WorkoutDetail from "../components/WorkoutDetail"
 import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
-    const [workouts, setWorkouts] = useState(null)
-
-    useEffect(() => {		
-		axios({
-			method: "GET",
-			url: "http://localhost:3000/api/workouts"
-		})
-		.then((res) => {
-				if (res.status === 200) {
-					setWorkouts(res.data)
-				}
-		})
-    },[])
-
+	const {data, isError, isFetching}  = useFetchWorkoutsQuery()
 
     return (
 	    <div className="home">
 		   <div className="workouts">
-				{workouts && workouts.map(workout => <WorkoutDetail workout={workout}/>)}	
+				{data && data.map(workout => <WorkoutDetail workout={workout} key={workout._id}/>)}	
 			</div>
 			<WorkoutForm/>
 		</div>
