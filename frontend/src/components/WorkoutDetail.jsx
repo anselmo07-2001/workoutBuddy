41bfa04ae583f4044074d7f097bb1591d380dbc3
Anselmo Rivera III <rivera.anselmo.2001@gmail.com>
2023-08-21 15:10:38 +0700

    added date-fns to date formatting

--- frontend/src/components/WorkoutDetail.jsx
+++ frontend/src/components/WorkoutDetail.jsx
@@ -1,5 +1,5 @@
 import { useDeleteWorkoutsMutation } from "../store"
-
+import formatDistanceToNow from "date-fns/formatDistanceToNow"
 
 const WorkoutDetail = ({workout}) => {
 	const [deleteWorkout, result] = useDeleteWorkoutsMutation()
@@ -13,7 +13,7 @@ const WorkoutDetail = ({workout}) => {
 			<h4>{workout.title}</h4>
 			<p><strong>Load (kg): </strong>{workout.load}</p>
 			<p><strong>Reps: </strong>{workout.reps}</p>
-			<p>{workout.createdAt}</p>
+			<p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
 			<span onClick={handleDelete}>delete</span>
 		</div>
 	)
